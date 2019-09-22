bae8f0c5d0435bddbcbb972034eb87a2fe93c0b5
Anand Chowdhary <mail@anandchowdhary.com>
2019-09-22 11:22:39 -0700

    :sparkles: Add speech recognition

--- live-subtitles.ts
+++ live-subtitles.ts
@@ -1,1 +1,25 @@
-console.log("OK");
+const recognition = new (window as any).webkitSpeechRecognition() as SpeechRecognition;
+recognition.continuous = true;
+recognition.interimResults = true;
+
+recognition.addEventListener("start", event => {
+  console.log("start", event);
+});
+recognition.addEventListener("result", event => {
+  console.log("result", event);
+});
+recognition.addEventListener("error", event => {
+  console.log("error", event);
+});
+recognition.addEventListener("end", event => {
+  console.log("end", event);
+});
+
+const startButton = document.querySelector("button");
+console.log(startButton);
+if (startButton) {
+  recognition.lang = "en-US";
+  startButton.addEventListener("click", () => {
+    recognition.start();
+  });
+}
